95ff81b987dcdc4ffc7ffd0ba746f85365baf997
khuongdinhtuan <khuongdinhtuan.hust@gmail.com>
2017-04-12 12:39:32 +0700

    day 1 - auth

---
+++ app/Http/Controllers/Admin/ManageController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers\Admin;
+
+//use App\Http\Request;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ManageController extends Controller
+{
+  public function index()
+  {
+    return "Today, i'm very happy";
+  }
+}
---
+++ app/Thread.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+  //
+  public function path()
+  {
+    return '/threads/' . $this->id;
+  }
+}
